bb87e06bbf3235e6f2df0316220c3eb4b1010d25
Siddharth Santosh <79411827+siddharthsantosh816@users.noreply.github.com>
2021-02-28 09:11:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -79,8 +79,9 @@ function setup() {
 function draw() {
   //trex.debug = true;
   background(170,206,251);
-  textSize = 20;
-  text("Score: "+ score, width-100, height/15);
+  textSize(18);
+  fill("blue");
+  text("Score: "+ score, width-120, height/15);
   text("High Score: "+ localStorage[0], 30, height/15);
   if (gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
